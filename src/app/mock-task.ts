a0d1f8ea5603c2eff2db4adc71260ce47e719249
Letilita <leticia.portillo.r@gmail.com>
2022-04-24 11:57:49 -0300

    Primer Commit

---
+++ src/app/mock-task.ts
@@ -0,0 +1,27 @@
+import {Task} from "./Task"
+export const TASK: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primero modulo de Angulas",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer compras para la semna",
+        day: "Agosto 5 a las 17:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar sobre Booststrap",
+        day: "Agosto 5 a las 12:00",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Leer mi libre favorito",
+        day: "Abril 8 a las 12:00",
+        reminder: true
+    }
+]
