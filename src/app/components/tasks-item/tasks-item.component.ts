6dc837fbd1cb07ffde60e2eca2349f8dd4d9e665
Letilita <leticia.portillo.r@gmail.com>
2022-04-24 12:44:14 -0300

    lista de  items

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {Task} from "../../Task";
+import {TASK} from "../../mock-task"
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent implements OnInit {
+
+  @Input() task: Task = TASK[0];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
