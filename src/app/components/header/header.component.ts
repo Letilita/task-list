4efe2b568372fa6b2b0c9db50af7e82413e726f3
Letilita <leticia.portillo.r@gmail.com>
2022-04-28 21:56:06 -0300

    lista como en el tutorial

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/service/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -13,14 +15,18 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean = false;
   subscription?: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router:Router) {
     this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask= value);
-;   }
+   }
 
   ngOnInit(): void {
   }
 
   toggleAddTask(){
     this.uiService.toggleAddTask()
+  }
+
+  hasRoute(route: string){
+    return this.router.url === "/";
   }
 }
